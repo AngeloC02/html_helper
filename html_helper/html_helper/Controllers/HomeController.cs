675ab4071acb8838ce66a352ea67404fd4666271
AngeloC02 <49086650+AngeloC02@users.noreply.github.com>
2019-03-30 19:53:32 -0400

    html_helper

---
+++ html_helper/html_helper/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using html_helper.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace html_helper.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            Persona objecto = new Persona();
+            objecto.Cedula = Convert.ToInt32(Request.Form["Cedula"]);
+            objecto.Nombre = (Request.Form["Nombre"]).ToString();
+            objecto.Apellido = (Request.Form["Apellido"]).ToString();
+            objecto.Edad = Convert.ToInt32(Request.Form["Edad"]);
+            objecto.Telefono = Convert.ToInt32(Request.Form["Telefono"]);
+            objecto.Correo = (Request.Form["Correo"]).ToString();
+            objecto.Genero = (Request.Form["Genero"]).ToString();
+            objecto.EstadoCivil = (Request.Form["EstadoCivil"]).ToString();
+            objecto.Hobbys = (Request.Form["Hobbys"]).ToString();
+            return View(objecto);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ html_helper/html_helper/Models/Persona.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace html_helper.Models
+{
+    public class Persona
+    {
+        [Required(ErrorMessage = "Este campo debe ser completado obligatoriamente")]
+        public int Cedula { get; set; }
+
+        [Required(ErrorMessage = "Este campo debe ser completado obligatoriamente")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Este campo debe ser completado obligatoriamente")]
+        public string Apellido { get; set; }
+
+        [Range(15,99, ErrorMessage = "La edad debe ser mayor o igual a 15 y menor de 99")]
+        public int Edad { get; set; }
+
+        public int Telefono { get; set; }
+
+        [EmailAddress(ErrorMessage = "Por favor introduceir un correo válido")]
+        public string Correo { get; set; }
+
+        public string Genero { get; set; }
+
+        public string EstadoCivil { get; set; }
+
+        public string Hobbys { get; set; }
+
+       
+    }
+}
